9a6f68b00a0a4a4889fbb89349a6cecd35c96e6c
79176 <79176@LAPTOP-MN0I58DS>
2023-09-07 19:35:36 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        /*задача 2*/
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            Console.WriteLine(b+" "+ a);
+        }
+    }
+}
